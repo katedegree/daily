e17568fcfb5167316231aa224515020781188ff8
Kate0418 <nakao.keito0418@gmail.com>
2026-04-05 22:32:11 +0900

    微修正

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,2 @@
 export * from "./cn"
 export * from "./today"
-export * from "./tomorrow"
--- src/utils/tomorrow.ts
+++
@@ -1,5 +0,0 @@
-export const tomorrow = () => {
-  const d = new Date();
-  d.setDate(d.getDate() + 1);
-  return d.toISOString().slice(0, 10).replace(/-/g, "");
-};
