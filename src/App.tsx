6389a55363dc3abc370fd2152341457134fa20d4
Kate0418 <nakao.keito0418@gmail.com>
2026-04-05 22:47:15 +0900

    保存処理完了

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useTaskStore } from "./use-task-store";
 import { Card } from "./components";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { today } from "./utils";
 
 function App() {
@@ -15,17 +15,37 @@ function App() {
     (t) => t.createdDate === today(),
   );
 
+  // 今のタスクの状態をlocalStorageのtasksに保存する。
+  const handleSave = () => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  };
+
+  // 初期ロード時にlocalStorageのtasksを参照する。
+  useEffect(() => {
+    const stored = localStorage.getItem("tasks");
+    if (!stored) return;
+    useTaskStore.setState({ tasks: JSON.parse(stored) });
+  }, []);
+
   return (
     <div className="h-screen grid grid-cols-[2fr_3fr_3fr] grid-rows-[auto_1fr] gap-x-4 p-8">
       <p className="pl-2 pb-2 text-sm">業務内容</p>
       <p className="pl-2 pb-2 text-sm">過去のTODO</p>
       <p className="pl-2 pb-2 text-sm">新規のTODO</p>
 
-      <textarea
-        className="w-full bg-board p-4 rounded-lg overflow-y-auto"
-        value={synopsis}
-        onChange={(e) => setSynopsis(e.target.value)}
-      />
+      <div className="grid grid-rows-[1fr_auto] gap-4">
+        <textarea
+          className="w-full bg-board p-4 rounded-lg overflow-y-auto"
+          value={synopsis}
+          onChange={(e) => setSynopsis(e.target.value)}
+        />
+        <div className="grid grid-cols-2 gap-4">
+          <button className="bg-input py-2 rounded-lg" onClick={handleSave}>
+            保存する
+          </button>
+          <button className="bg-input py-2 rounded-lg">日報をコピー</button>
+        </div>
+      </div>
 
       <div className="flex flex-col gap-4 overflow-y-auto">
         {remainingTasks.map((task) => {
